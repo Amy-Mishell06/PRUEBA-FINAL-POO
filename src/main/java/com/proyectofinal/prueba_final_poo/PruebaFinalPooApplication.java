e84e6810708edcc06cb324da2db73421adeca4ed
MaCaReNo <suarezalex833@gmail.com>
2026-01-15 21:19:09 -0500

    Avance del proyecto final

---
+++ src/main/java/com/proyectofinal/prueba_final_poo/PruebaFinalPooApplication.java
@@ -0,0 +1,13 @@
+package com.proyectofinal.prueba_final_poo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PruebaFinalPooApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PruebaFinalPooApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/proyectofinal/prueba_final_poo/PruebaFinalPooApplicationTests.java
@@ -0,0 +1,13 @@
+package com.proyectofinal.prueba_final_poo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PruebaFinalPooApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
