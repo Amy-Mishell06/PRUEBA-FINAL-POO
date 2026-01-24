b0b07bba0a57bc1ed077d94689beed3c7d88bd36
MaCaReNo <suarezalex833@gmail.com>
2026-01-24 18:41:23 -0500

    Agrega manejo global de errores y validaciones

--- src/main/java/com/proyectofinal/prueba_final_poo/exception/GlobalExceptionHandler.java
+++ src/main/java/com/proyectofinal/prueba_final_poo/exception/GlobalExceptionHandler.java
@@ -4,6 +4,9 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import java.util.HashMap;
+import java.util.Map;
 
 // Maneja excepciones de forma global
 @ControllerAdvice
@@ -13,5 +16,18 @@ public class GlobalExceptionHandler {
     @ExceptionHandler(ResourceNotFoundException.class)
     public ResponseEntity<String> manejarRecursoNoEncontrado(ResourceNotFoundException ex) {
         return new ResponseEntity<>(ex.getMessage(), HttpStatus.NOT_FOUND);
+    }
+
+    //Errores de validacion
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<Map<String, String>> manejarValidaciones(MethodArgumentNotValidException ex) {
+
+        Map<String, String> errores = new HashMap<>();
+
+        ex.getBindingResult().getFieldErrors().forEach(error ->
+                errores.put(error.getField(), error.getDefaultMessage())
+        );
+
+        return new ResponseEntity<>(errores, HttpStatus.BAD_REQUEST);
     }
 }
